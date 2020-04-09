65bb6847d94f4b3c11614afec16a9527b9d2f8a6
logicstark <logicalstark1105@gmail.com>
2020-04-09 16:38:33 +0900

    create LinkedList

---
+++ CollectionFramework/src/linkedlistclass/Remove.java
@@ -0,0 +1,25 @@
+package linkedlistclass;
+
+import java.util.LinkedList;
+
+public class Remove {
+    public static void main(String[] args) {
+        LinkedList<String> fruitList = new LinkedList<>();
+
+        fruitList.add("Apple");
+        fruitList.add("banana");
+        fruitList.add("mango");
+        fruitList.add("Pinaple");
+
+
+
+        fruitList.clear();
+        System.out.println("Cleared the LinkedList => " + fruitList);
+
+
+
+
+
+
+    }
+}
