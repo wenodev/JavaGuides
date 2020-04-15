d1d25f9cdd283a9cdc412e6ecbe365c6003413e9
logicstark <logicalstark1105@gmail.com>
2020-04-15 12:31:24 +0900

    Stream filter

---
+++ Stream/src/withoutStream/JavaStreamExample.java
@@ -0,0 +1,33 @@
+package withoutStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaStreamExample {
+
+    private static List< Product > productsList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        // Adding Products
+        productsList.add(new Product(1, "HP Laptop", 25000f));
+        productsList.add(new Product(2, "Dell Laptop", 30000f));
+        productsList.add(new Product(3, "Lenevo Laptop", 28000f));
+        productsList.add(new Product(4, "Sony Laptop", 28000f));
+        productsList.add(new Product(5, "Apple Laptop", 90000f));
+        // With Java 8 Stream API'S
+        withStreamAPI();
+    }
+
+    private static void withStreamAPI() {
+        // filtering data of list
+        List < Float > productPriceList = productsList.stream().filter((product) -> product.getPrice() > 25000)
+                .map((product) -> product.getPrice()).collect(Collectors.toList());
+        // displaying data
+        productPriceList.forEach((price) -> System.out.println(price));
+    }
+
+
+
+}
