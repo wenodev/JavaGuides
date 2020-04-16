80b7744e29b1675f1e9998111f054c31fd405bce
logicstark <logicalstark1105@gmail.com>
2020-04-16 18:44:54 +0900

    create di

---
+++ springmvc5/src/main/java/weno/di/AppConfiguration.java
@@ -0,0 +1,11 @@
+package weno.di;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("weno.di")
+public class AppConfiguration {
+
+}
---
+++ springmvc5/src/main/java/weno/di/TwitterService.java
@@ -0,0 +1,10 @@
+package weno.di;
+
+import org.springframework.stereotype.Service;
+
+@Service("TwitterService")
+public class TwitterService implements MessageService {
+    public void sendMsg(String message) {
+        System.out.println(message);
+    }
+}
