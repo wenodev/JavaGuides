64e97aeea32c118149778e6464439db9c2706564
logicstark <logicalstark1105@gmail.com>
2020-04-22 13:21:12 +0900

    create Crud RestApi Tutorial

---
+++ MySQL CRUD REST API Tutorial/src/main/java/com/example/demo/MySqlCrudRestApiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlCrudRestApiTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlCrudRestApiTutorialApplication.class, args);
+    }
+
+}
