9fe282ded00d07375124d33cc4483e490c6a9d73
logicstark <logicalstark1105@gmail.com>
2020-04-29 17:09:01 +0900

    create RoleBasedSecuritywithJPA

---
+++ SpringSecurity/RoleBasedSecuritywithJPA/src/main/java/com/RoleBasedSecuritywithJPA/RoleBasedSecuritywithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.RoleBasedSecuritywithJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedSecuritywithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedSecuritywithJpaApplication.class, args);
+	}
+
+}
