62094bef7bf1bcb723b775e2cbcf13d29f4fc1a0
logicstark <logicalstark1105@gmail.com>
2020-05-07 20:59:01 +0900

    RoleBasedSecuritywithJpa done

--- SpringSecurity/RoleBasedSecuritywithJPA/src/main/java/com/RoleBasedSecuritywithJPA/web/HomeController.java
+++ SpringSecurity/RoleBasedSecuritywithJPA/src/main/java/com/RoleBasedSecuritywithJPA/web/HomeController.java
@@ -1,4 +1,29 @@
 package com.RoleBasedSecuritywithJPA.web;
 
+import com.RoleBasedSecuritywithJPA.entities.Message;
+import com.RoleBasedSecuritywithJPA.repositories.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class HomeController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("msgs", messageRepository.findAll());
+        return "userhome";
+    }
+
+    @PostMapping("/messages")
+    public String saveMessage(Message message){
+        messageRepository.save(message);
+        return "redirect:/home";
+    }
+
 }
