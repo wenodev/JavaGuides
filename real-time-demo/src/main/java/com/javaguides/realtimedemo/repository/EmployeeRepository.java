5ea9cbde6e6ce821dfd17708dc5c2e7534c55412
logicstark <logicalstark1105@gmail.com>
2020-09-10 18:14:53 +0900

    delete 0910

--- real-time-demo/src/main/java/com/javaguides/realtimedemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaguides.realtimedemo.repository;
-
-import com.javaguides.realtimedemo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
